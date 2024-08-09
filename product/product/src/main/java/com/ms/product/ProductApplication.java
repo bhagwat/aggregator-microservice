35b5d71d0aca160496633fd68a62f0c626e53e0b
Bhagwat Kumar <bhagwat.kumar@pwc.com>
2024-08-09 11:11:41 +0530

    Removed utility class CsvDataLoader.java and its usages

--- product/product/src/main/java/com/ms/product/ProductApplication.java
+++ product/product/src/main/java/com/ms/product/ProductApplication.java
@@ -1,6 +1,5 @@
 package com.ms.product;
 
-import com.ms.product.repository.ProductRepository;
 import com.ms.product.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
@@ -10,15 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         var ctx = SpringApplication.run(ProductApplication.class, args);
         ProductService productService = ctx.getBean(ProductService.class);
-        productService.importFromCsv();
-
-        ProductRepository productRepository = ctx.getBean(ProductRepository.class);
-        productRepository.findAll().forEach(product -> {
-                    log.info(String.format("Product ID: %s Name: %s , Desc %s", product.getId(), product.getName(), product.getDescription()));
-                }
-        );
+        productService.bootstrap();
     }
 }

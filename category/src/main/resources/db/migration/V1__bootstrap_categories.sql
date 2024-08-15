a1867859ae90e2e9a55ac3c19486bfcf94e1ba02
bhagwatkumar <radialgoal@gmail.com>
2024-08-15 14:39:58 +0530

    Bootstrapped more categories to be used in Product micro-service

--- category/src/main/resources/db/migration/V1__bootstrap_categories.sql
+++ category/src/main/resources/db/migration/V1__bootstrap_categories.sql
@@ -16,13 +16,18 @@ INSERT INTO categories (id, name, parent_category_id) VALUES ('electronics', 'El
 INSERT INTO categories (id, name, parent_category_id) VALUES ('computers', 'Computers', 'electronics');
 INSERT INTO categories (id, name, parent_category_id) VALUES ('laptops', 'Laptops', 'computers');
 INSERT INTO categories (id, name, parent_category_id) VALUES ('desktops', 'Desktops', 'computers');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('camera', 'Camera', 'electronics');
 INSERT INTO categories (id, name, parent_category_id) VALUES ('smartphones', 'Smartphones', 'electronics');
 
 -- Subcategories of Accessories (under Electronics)
 INSERT INTO categories (id, name, parent_category_id) VALUES ('accessories', 'Accessories', 'electronics');
 INSERT INTO categories (id, name, parent_category_id) VALUES ('mouse', 'Mouse', 'accessories');
 INSERT INTO categories (id, name, parent_category_id) VALUES ('speaker', 'Speaker', 'accessories');
-INSERT INTO categories (id, name, parent_category_id) VALUES ('joystick', 'Joystick', 'accessories');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('gaming', 'Gaming', 'accessories');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('monitor', 'Monitor', 'accessories');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('keyboard', 'Keyboard', 'accessories');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('network', 'Network', 'accessories');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('storage', 'Storage', 'accessories');
 
 -- Subcategories of Mouse (under Accessories)
 INSERT INTO categories (id, name, parent_category_id) VALUES ('wiredMouse', 'Wired Mouse', 'mouse');
@@ -31,3 +36,8 @@ INSERT INTO categories (id, name, parent_category_id) VALUES ('bluetoothMouse', 
 -- Subcategories of Speaker (under Accessories)
 INSERT INTO categories (id, name, parent_category_id) VALUES ('headset', 'Headset', 'speaker');
 INSERT INTO categories (id, name, parent_category_id) VALUES ('headphone', 'Headphone', 'speaker');
+
+-- Subcategories of smartphones
+INSERT INTO categories (id, name, parent_category_id) VALUES ('smartwatch', 'Smart watch', 'smartphones');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('android', 'Android Smart Phone', 'smartphones');
+INSERT INTO categories (id, name, parent_category_id) VALUES ('iphone', 'Apple IPhone', 'smartphones');

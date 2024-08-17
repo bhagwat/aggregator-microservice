a3f3d4e7cb294d32da8ae64e63e955b4ef709bde
bhagwatkumar <radialgoal@gmail.com>
2024-08-17 10:22:17 +0530

    Order Microservice - Moved bootstrap code to separate Service

--- order/src/main/java/com/ms/order/OrderApplication.java
+++ order/src/main/java/com/ms/order/OrderApplication.java
@@ -1,6 +1,12 @@
+/*
+ * Copyright (c) 2024.
+ *
+ * @author Bhagwat Kumar
+ */
+
 package com.ms.order;
 
-import com.ms.order.service.OrderService;
+import com.ms.order.service.BootstrapService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +17,6 @@ public class OrderApplication {
 
     public static void main(String[] args) throws Exception {
         var ctx = SpringApplication.run(OrderApplication.class, args);
-        ctx.getBean(OrderService.class).bootstrap();
+        ctx.getBean(BootstrapService.class).bootstrap();
     }
 }

32fc15faefb815259d4e30cbc21e32916cd30108
bhagwatkumar <radialgoal@gmail.com>
2024-08-17 10:25:10 +0530

    Order Microservice - API enhancement by moving repo code to service layer + support for pagination in /all API + A new API to get orders by list of IDs

--- order/src/main/java/com/ms/order/controller/OrderController.java
+++ order/src/main/java/com/ms/order/controller/OrderController.java
@@ -1,33 +1,70 @@
+/*
+ * Copyright (c) 2024.
+ *
+ * @author Bhagwat Kumar
+ */
+
 package com.ms.order.controller;
 
 import com.ms.order.entity.Order;
-import com.ms.order.repository.OrderRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ms.order.errorHandler.OrderNotFoundException;
+import com.ms.order.service.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/order")
 public class OrderController {
-    private final OrderRepository orderRepository;
+    private final OrderService orderService;
 
-    public OrderController(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
     }
 
+    /**
+     * Get order by ID
+     *
+     * @param id Order ID
+     * @return Order instance or Order not found response
+     */
     @GetMapping("/{id}")
-    Order findById(@PathVariable Long id) {
-        System.out.println("findById:: " + id);
-        return orderRepository.findById(id)
-                .orElse(new Order());
+    Order findOrderById(@PathVariable Long id) {
+        log.info("OrderController.findById:: {}", id);
+        return orderService.findById(id).orElseThrow(() -> new OrderNotFoundException(id));
     }
 
+    /**
+     * Get orders by given IDs
+     *
+     * @param ids List of order IDs
+     * @return List of Order
+     */
     @GetMapping
-    List<Order> findAll() {
-        return orderRepository.findAll();
+    List<Order> findOrdersByIds(@RequestParam List<Long> ids) {
+        log.info("OrderController.findOrdersByIds:: {}", ids);
+        return orderService.findAllByIds(ids);
     }
 
+    /**
+     * Get orders with pagination support
+     *
+     * @return List of Orders matching the pagination params
+     */
+    @GetMapping("/all")
+    Page<Order> findAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int pageSize,
+            @RequestParam(defaultValue = "lastUpdated,desc") String[] sort) {
+        String sortField = sort[0];
+        Sort.Direction direction = sort[1].equalsIgnoreCase("desc") ? Sort.Direction.DESC : Sort.Direction.ASC;
+        Pageable pageable = PageRequest.of(page, pageSize, Sort.by(direction, sortField));
+        return orderService.findAll(pageable);
+    }
 }
